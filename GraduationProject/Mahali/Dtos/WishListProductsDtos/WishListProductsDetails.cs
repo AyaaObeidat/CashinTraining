68d463dedb3261f000295711c8d22a47bb94675c
AYA OBEIDAT <amobeidat208@cit.just.edu.jo>
2024-05-12 15:09:59 +0300

    save changes

---
+++ GraduationProject/Mahali/Dtos/WishListProductsDtos/WishListProductsDetails.cs
@@ -0,0 +1,11 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Mahali.Dtos.WishListProductsDtos
+{
+    public class WishListProductsDetails
+    {
+        public Guid Id { get; set; }
+        public Guid WishListId { get;  set; }
+        public Guid ProductId { get;  set; }
+    }
+}
