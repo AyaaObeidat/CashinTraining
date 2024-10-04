af20e06b2714042b46aa06716074395bd05344fb
AYA OBEIDAT <amobeidat208@cit.just.edu.jo>
2024-10-04 23:26:59 +0300

    save

--- Angular-Task-2/MyProject/src/app/app.component.ts
+++ Angular-Task-2/MyProject/src/app/app.component.ts
@@ -1,15 +1,25 @@
-import { Component } from '@angular/core';
+import { AfterViewInit, Component, CUSTOM_ELEMENTS_SCHEMA, NO_ERRORS_SCHEMA, OnInit } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { EvenMinutesDirective } from './even-minutes.directive';
+import { DatePipe } from '@angular/common';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, EvenMinutesDirective],
+  imports: [RouterOutlet, EvenMinutesDirective,DatePipe],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css',
+  schemas: [CUSTOM_ELEMENTS_SCHEMA]
 })
-export class AppComponent {
+export class AppComponent implements OnInit{
   myDate = new Date();
+  isActive = false;
   minutes = this.myDate.getMinutes();
+  constructor() {
+   
+  }
+  ngOnInit(): void {
+    if(this.minutes%2 == 0)
+      this.isActive = true;
+  }
 }
--- Angular-Task-2/MyProject/src/app/even-minutes.directive.ts
+++ Angular-Task-2/MyProject/src/app/even-minutes.directive.ts
@@ -1,21 +1,21 @@
-import { Directive, Input, TemplateRef, ViewContainerRef } from '@angular/core';
+import { Directive, Input, OnInit, TemplateRef, ViewContainerRef } from '@angular/core';
 
 @Directive({
   selector: '[appEvenMinutes]',
   standalone: true,
 })
 export class EvenMinutesDirective {
-  @Input('appEvenMinutes')
-  set CheckMinutes(m: number) {
-    if (m == null || m == undefined) {
-      this.vcRef.clear();
-    } else {
-      if (m % 2 == 0) this.vcRef.createEmbeddedView(this.temRef);
-      else this.vcRef.clear();
-    }
+ @Input('appEvenMinutes') set SetElement(c : boolean)
+ {
+  if(c)
+  {
+    this.vcRef.createEmbeddedView(this.temRef);
   }
+ }
   constructor(
     private temRef: TemplateRef<any>,
     private vcRef: ViewContainerRef
   ) {}
+
+ 
 }
