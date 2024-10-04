fff0a128934a278435c41652e87ed7082020a97d
AYA OBEIDAT <amobeidat208@cit.just.edu.jo>
2024-10-04 20:45:19 +0300

    save

--- Task-1/SchoolSystem/obj/Debug/net6.0/SchoolSystem.AssemblyInfo.cs
+++ Task-1/SchoolSystem/obj/Debug/net6.0/SchoolSystem.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("SchoolSystem")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+201cfa3b8194372bdc96a0be8ae6a3714aedb23a")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+325a1a449a02d628bbde8f5866945ca94b5e3b4b")]
 [assembly: System.Reflection.AssemblyProductAttribute("SchoolSystem")]
 [assembly: System.Reflection.AssemblyTitleAttribute("SchoolSystem")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
