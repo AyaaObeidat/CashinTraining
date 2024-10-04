4ecbdb881c690013b3d5d71a4905bf62ec244473
AYA OBEIDAT <amobeidat208@cit.just.edu.jo>
2024-10-04 21:08:12 +0300

    save

--- Angular-Task-2/MyProject/src/app/app.component.ts
+++ Angular-Task-2/MyProject/src/app/app.component.ts
@@ -1,13 +1,15 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { EvenMinutesDirective } from './even-minutes.directive';
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, EvenMinutesDirective],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
 export class AppComponent {
-  title = 'MyProject';
+  myDate = new Date();
+  minutes = this.myDate.getMinutes();
 }
---
+++ Angular-Task-2/MyProject/src/app/even-minutes.directive.spec.ts
@@ -0,0 +1,8 @@
+import { EvenMinutesDirective } from './even-minutes.directive';
+
+describe('EvenMinutesDirective', () => {
+  it('should create an instance', () => {
+    const directive = new EvenMinutesDirective();
+    expect(directive).toBeTruthy();
+  });
+});
