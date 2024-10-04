201cfa3b8194372bdc96a0be8ae6a3714aedb23a
AYA OBEIDAT <amobeidat208@cit.just.edu.jo>
2024-10-04 20:20:25 +0300

    save all

--- src/app/evennumbs.pipe.spec.ts
+++
@@ -1,8 +0,0 @@
-import { EvennumbsPipe } from './evennumbs.pipe';
-
-describe('EvennumbsPipe', () => {
-  it('create an instance', () => {
-    const pipe = new EvennumbsPipe();
-    expect(pipe).toBeTruthy();
-  });
-});
