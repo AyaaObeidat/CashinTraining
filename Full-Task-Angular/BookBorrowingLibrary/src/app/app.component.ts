ec4f4989421104cbbbf406834025778a2dabe98a
AYA OBEIDAT <amobeidat208@cit.just.edu.jo>
2024-10-17 10:47:41 +0300

    SAVE

--- Full-Task-Angular/BookBorrowingLibrary/src/app/app.component.ts
+++ Full-Task-Angular/BookBorrowingLibrary/src/app/app.component.ts
@@ -2,6 +2,7 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { UserService } from './services/user.service';
 import { HttpClientModule } from '@angular/common/http';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-root',
--- Full-Task-Angular/BookBorrowingLibrary/src/app/components/Customer/customer-profile/customer-profile.component.ts
+++ Full-Task-Angular/BookBorrowingLibrary/src/app/components/Customer/customer-profile/customer-profile.component.ts
@@ -1,20 +1,27 @@
-import { Component, Inject, OnDestroy, OnInit, Renderer2 } from '@angular/core';
+import { Component,Inject, OnDestroy, OnInit, Renderer2 } from '@angular/core';
 import { CustomerHeaderComponent } from '../customer-header/customer-header.component';
 import { CommonModule, DOCUMENT } from '@angular/common';
 import { CustomerServService } from '../customerServ/customer-serv.service';
+import { FormsModule } from '@angular/forms';
 @Component({
   selector: 'app-customer-profile',
   standalone: true,
-  imports: [CustomerHeaderComponent,CommonModule],
+  imports: [CustomerHeaderComponent,CommonModule,FormsModule],
   templateUrl: './customer-profile.component.html',
   styleUrl: './customer-profile.component.css',
 })
 export class CustomerProfileComponent implements OnInit, OnDestroy {
   customer : any;
+  newFullName?: string;
+  currentPassword?:string;
+  newPassword?:string;
+  confirmPassword?:string;
+  newPhoneNumber?:number;
+  toEdit = false;
   constructor(
     @Inject(DOCUMENT) private document: Document,
     private render: Renderer2,
-    private customerServ : CustomerServService
+    private customerServ : CustomerServService,
   ) {
     this.customer = this.customerServ.GetCustomerData();
   }
@@ -23,5 +30,15 @@ export class CustomerProfileComponent implements OnInit, OnDestroy {
   }
   ngOnDestroy(): void {
      this.render.removeStyle(this.document.body,'backgroundColor');
+  }
+
+  ClickToEdit()
+  {
+    this.toEdit = true;
+  }
+  ClickToSave()
+  {
+    this.toEdit = false;
+    
   }
 }
